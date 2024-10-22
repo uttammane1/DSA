db2e3dcffdef0ebbd5b6a8cad7ff6943d1883959
uttammane1 <uttammaneofficial@gmail.com>
2024-10-22 12:05:14 +0530

    Reversing an Array

---
+++ Time Complexity/Reversing_an_Array.py
@@ -0,0 +1,11 @@
+def reverse_array(arr):
+    start = 0
+    end = len(arr) - 1
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+    return arr
+
+arr = [1, 2, 3, 4, 5]
+print(reverse_array(arr))  
