27a89eea111da3b312f171ad8e43c701264afa80
uttammane1 <uttammaneofficial@gmail.com>
2024-10-23 15:07:23 +0530

    Binary Search

---
+++ Time Complexity/Binary_Search.py
@@ -0,0 +1,16 @@
+def binary_search(arr, low, high, x):
+    if high >= low:
+        mid = (high + low) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return binary_search(arr, low, mid - 1, x)
+        else:
+            return binary_search(arr, mid + 1, high, x)
+    else:
+        return -1
+
+arr = [2, 3, 4, 10, 40]
+x = 10
+result = binary_search(arr, 0, len(arr) - 1, x)
+print("Element found at index:", result)  
