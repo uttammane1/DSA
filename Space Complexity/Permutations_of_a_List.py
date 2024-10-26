277b9780217ca30723f75c02d4f92ba6e54d4d99
uttammane1 <uttammaneofficial@gmail.com>
2024-10-26 10:14:38 +0530

    Permutations of a List

---
+++ Space Complexity/Permutations_of_a_List.py
@@ -0,0 +1,16 @@
+def permute(nums):
+    result = []
+
+    def backtrack(start):
+        if start == len(nums):
+            result.append(nums[:])
+        for i in range(start, len(nums)):
+            nums[start], nums[i] = nums[i], nums[start]
+            backtrack(start + 1)
+            nums[start], nums[i] = nums[i], nums[start]
+
+    backtrack(0)
+    return result
+
+nums = [1, 2, 3]
+print("Permutations:", permute(nums))
