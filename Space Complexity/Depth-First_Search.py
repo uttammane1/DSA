998d3a8e5bc374e650b11df21b164a9b07e10a5d
uttammane1 <uttammaneofficial@gmail.com>
2024-10-26 10:13:33 +0530

    Depth-First Search

---
+++ Space Complexity/Depth-First_Search.py
@@ -0,0 +1,18 @@
+def dfs(graph, node, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(node)
+    for neighbor in graph[node]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+    return visited
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+print("DFS:", dfs(graph, 'A'))
