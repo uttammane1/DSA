980016c3aaad2e2a5d35c22cf2247a78d3c1499b
uttammane1 <uttammaneofficial@gmail.com>
2024-10-26 10:10:34 +0530

    Matrix Multiplication

---
+++ Space Complexity/Matrix_Multiplication.py
@@ -0,0 +1,20 @@
+def matrix_multiply(A, B):
+    rows_A = len(A)
+    cols_A = len(A[0])
+    rows_B = len(B)
+    cols_B = len(B[0])
+
+    if cols_A != rows_B:
+        raise ValueError("Matrices cannot be multiplied.")
+
+    result = [[0 for _ in range(cols_B)] for _ in range(rows_A)]
+
+    for i in range(rows_A):
+        for j in range(cols_B):
+            for k in range(cols_A):
+                result[i][j] += A[i][k] * B[k][j]
+    return result
+
+A = [[1, 2, 3], [4, 5, 6]]
+B = [[7, 8], [9, 10], [11, 12]]
+print("Result:", matrix_multiply(A, B))
