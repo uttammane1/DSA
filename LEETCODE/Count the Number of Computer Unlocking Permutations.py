a1d2a2fa7f55310f94e3b3769fe2cdbb323fa1dd
uttammane1 <uttammaneofficial@gmail.com>
2025-12-10 23:43:44 +0530

    count

--- LEETCODE/Count the Number of Computer Unlocking Permutations.py
+++ LEETCODE/Count the Number of Computer Unlocking Permutations.py
@@ -15,7 +15,7 @@ class Solution(object):
             for j in range(1, i - 1):
                 prefix[j] = (prefix[j - 1] + dp[i - 1][j]) % MOD
 
-            if complexity[i - 2] == 1:  # increasing
+            if complexity[i - 2] == 1:  
                 for j in range(i):
                     if j > 0:
                         dp[i][j] = prefix[j - 1]
