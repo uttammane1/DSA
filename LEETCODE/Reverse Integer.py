e21258a8e0a1aef46860b26253f8d38579820898
uttammane1 <uttammaneofficial@gmail.com>
2025-12-14 00:51:20 +0530

    Reverse

---
+++ LEETCODE/Reverse Integer.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+        INT_MIN = -2**31
+        INT_MAX = 2**31 - 1
+
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        rev = 0
+        while x != 0:
+            pop = x % 10
+            x //= 10
+
+            # Check overflow
+            if rev > (INT_MAX - pop) // 10:
+                return 0
+
+            rev = rev * 10 + pop
+
+        return sign * rev
